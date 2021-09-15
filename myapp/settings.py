2aa088c145819a70172ca1c2c22862bf5e1ca226
Sarthak <sarthak.tuteja@yahoo.com>
2021-09-16 04:04:31 +0530

    added to allowed host

--- myapp/settings.py
+++ myapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y^5+or*4wy-b=wia_(3i!$m1%#7)0t3dk_=#%*bqsgfa5k2r!s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thrivefund.herokuapp.com']
 
 
 # Application definition
