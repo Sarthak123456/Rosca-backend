b886edb5f89ba6720c4d760656be91a6afed349b
Sarthak <sarthak.tuteja@yahoo.com>
2022-01-04 16:31:18 +0530

    db changes

--- myapp/settings.py
+++ myapp/settings.py
@@ -93,23 +93,23 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 #
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME':  env('DATABASE_NAME'),
-#         'USER':  env('DATABASE_USER'),
-#         'PASSWORD':  env('DATABASE_PASS'),
-#         'HOST': 'ec2-44-199-26-122.compute-1.amazonaws.com',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':  env('DATABASE_NAME'),
+        'USER':  env('DATABASE_USER'),
+        'PASSWORD':  env('DATABASE_PASS'),
+        'HOST': 'ec2-44-199-26-122.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
